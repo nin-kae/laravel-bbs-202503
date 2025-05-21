ab1e49211e5d1b94a6e657f5cf88878f34b4f4ec
nin-kae <renhuaying0401@gmail.com>
2025-05-21 18:38:49 +0900

    基础页面结构

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function root()
+    public function root(): View
     {
+        /**
+         * Show the application dashboard.
+         *
+         * @return View
+         */
         return view('pages.root');
     }
 }
--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,1 +1,9 @@
-<?php
+@foreach (['danger', 'warning', 'success', 'info'] as $message)
+    @if(session()->has($message))
+        <div class="flash-message">
+            <p class="alert alert-{{ $message }}">
+                {{ session()->get($message) }}
+            </p>
+        </div>
+    @endif
+@endforeach
