3b91bd0760535c5855829cd01b362b88c2c8c67b
nin-kae <renhuaying0401@gmail.com>
2025-06-04 00:19:59 +0900

    发布话题的时候自动生成 slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -14,7 +14,17 @@ class TopicObserver
      */
     public function saving(Topic $topic): void
     {
+        // 过滤话题内容的特殊标签
         $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 生成摘要
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如果没有 slug，则使用标题生成 slug
+        // 我们按照查得的日本公司最常见的方式来处理 slug
+        // 生成 slug 对 URL 友好，一般是百分比符号数字字母横杠下划线组成，防止URL出现特殊符号导致网页无法识别
+        if (!$topic->slug) {
+            $topic->slug = rawurlencode($topic->title);
+        }
     }
 }
