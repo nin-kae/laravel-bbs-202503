57bb26ea252defc493149c7f3ee47268c889a289
nin-kae <renhuaying0401@gmail.com>
2025-06-19 16:03:02 +0900

    侧边栏链接，资源推荐

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -6,7 +6,6 @@
     </div>
 </div>
 
-@isset($active_users)
 @if (count($active_users))
 <div class="card mt-4">
     <div class="card-body active-users pt-2">
@@ -25,4 +24,21 @@
     </div>
 </div>
 @endif
-@endisset
+
+@if (count($links))
+<div class="card mt-4">
+    <div class="card-body pt-2">
+        <div class="text-center mt-1 mb-0 text-muted">
+            {{ __('Recommended Resources') }}
+        </div>
+        <hr class="mt-2 mb-3">
+        @foreach ($links as $link)
+            <a class="d-flex mt-1 text-decoration-none" href="{{ $link->link }}">
+                <div class="media-body mt-2">
+                    <span class="media-heading text-muted">{{ $link->title }}</span>
+                </div>
+            </a>
+        @endforeach
+    </div>
+</div>
+@endif
